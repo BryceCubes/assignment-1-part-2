4f37c85a1e34126de30beb6332b4846467419f0e
Zhichao Chen <zhichaochen@login-students.ccs.neu.edu>
2020-01-30 11:53:14 -0500

    Modify test so it works with gtest, modify cmakelists to make sure it's compatable with version 2.8

--- test-queue.cpp
+++ test-queue.cpp
@@ -6,116 +6,166 @@
 
 // test basic behaviors
 void Test1() {
-    bool result = true;
-    StrQueue* q = new StrQueue(5);
-    // test allocated size and current size
-    result &= (q->current_size() == 0);
-    result &= (q->allocated_size() == 5);
+  bool result = true;
+  StrQueue* q = new StrQueue(5);
+  // test allocated size and current size
+  result &= (q->current_size() == 0);
+  result &= (q->allocated_size() == 5);
 
-    String* o = new String("hello");
-    String* o2 = new String("world");
+  String* o = new String("hello");
+  String* o2 = new String("world");
 
-    // test enqueue
-    q->enqueue(o);
-    q->enqueue(o2);
-    result &= (q->current_size() == 2);
-    result &= (q->allocated_size() == 5);
+  // test enqueue
+  q->enqueue(o);
+  q->enqueue(o2);
+  result &= (q->current_size() == 2);
+  result &= (q->allocated_size() == 5);
 
-    // test peek
-    result &= (q->peek()->equals(o));
-    result &= (q->current_size() == 2);
-    result &= (q->allocated_size() == 5);
+  String* s = q->peek();
 
-    // test dequeue
-    result &= (q->dequeue()->equals(o));
-    result &= (q->current_size() == 1);
-    result &= (q->allocated_size() == 5);
+  // test peek
+  result &= (s->equals(o));
+  result &= (q->current_size() == 2);
+  result &= (q->allocated_size() == 5);
 
-    // test clear
-    q->clear();
-    result &= (!(q->isFull()));
-    result &= (q->isEmpty());
+  delete s;
 
-    // test is full and is empty
-    q->enqueue(o);
-    q->enqueue(o);
-    q->enqueue(o);
-    q->enqueue(o);
-    q->enqueue(o);
+  String* st = q->dequeue();
 
-    result &= (q->current_size() == 5);
-    result &= (q->allocated_size() == 5);
+  // test dequeue
+  result &= (st->equals(o));
+  result &= (q->current_size() == 1);
+  result &= (q->allocated_size() == 5);
 
-    result &= (q->isFull());
-    result &= (!(q->isEmpty()));
-    printf("%d\n", result);
-    delete q;
-    delete o;
+  delete st;
+
+  // test clear
+  q->clear();
+  result &= (!(q->isFull()));
+  result &= (q->isEmpty());
+
+  // test is full and is empty
+  q->enqueue(o);
+  q->enqueue(o);
+  q->enqueue(o);
+  q->enqueue(o);
+  q->enqueue(o);
+
+  result &= (q->current_size() == 5);
+  result &= (q->allocated_size() == 5);
+
+  result &= (q->isFull());
+  result &= (!(q->isEmpty()));
+  delete q;
+  delete o;
+  delete o2;
+  if (result) {
+    exit(0);
+  }
+  exit(4);
+}
+
+TEST(W1, Test1) {
+  ASSERT_EXIT(Test1(), ::testing::ExitedWithCode(0), ".*");
 }
 
 // test enqueue more elements than allocated size
 void Test2() {
-    Queue* q = new Queue(5);
-    
-    Object* o = new Object();
+  Queue* q = new Queue(5);
 
-    q->enqueue(o);
-    q->enqueue(o);
-    q->enqueue(o);
-    q->enqueue(o);
-    q->enqueue(o);
-    ASSERT_EXIT(q->enqueue(o), ::testing::ExitedWithCode(1));
+  Object* o = new Object();
 
-    delete q;
-    delete o;
+  q->enqueue(o);
+  q->enqueue(o);
+  q->enqueue(o);
+  q->enqueue(o);
+  q->enqueue(o);
+  q->enqueue(o);
+
+  delete q;
+  delete o;
+  exit(0);
+}
+
+TEST(W1, Test2) {
+  ASSERT_EXIT(Test2(), ::testing::ExitedWithCode(1), ".*");
 }
 
 // test dequeue empty queue
 void Test3() {
-    Queue* q = new Queue(5);
-    
-    ASSERT_EXIT(q->dequeue(), ::testing::ExitedWithCode(1));
+  Queue* q = new Queue(5);
 
-    delete q;
+  q->dequeue();
+
+  delete q;
+  exit(0);
+}
+
+TEST(W1, Test3) {
+  ASSERT_EXIT(Test3(), ::testing::ExitedWithCode(2), ".*");
 }
 
 // test peek empty queue
 void Test4() {
-    Queue* q = new Queue(5);
-    
-    ASSERT_EXIT(q->peek(), ::testing::ExitedWithCode(1));
+  Queue* q = new Queue(5);
 
-    delete q;
+  q->peek();
+
+  delete q;
+  exit(0);
+}
+
+TEST(W1, Test4) {
+  ASSERT_EXIT(Test4(), ::testing::ExitedWithCode(3), ".*");
 }
 
 // test equal and hash
 void Test5() {
-    bool result = true;
-    StrQueue* q1 = new StrQueue(5);
-    StrQueue* q2 = new StrQueue(4);
-    
-    String* o = new String("hello");
+  bool result = true;
+  StrQueue* q1 = new StrQueue(5);
+  StrQueue* q2 = new StrQueue(4);
 
-    q1->enqueue(o);
-    q2->enqueue(o);
+  String* o = new String("hello");
+  String* o2 = new String("goodbye");
 
-    result &= (q1->equals(q2));
-    result &= (q1->hash() == q2->hash());
-    
-    printf("%d\n", result);
-    
-    delete q1;
-    delete q2;
-    delete o;
+  q1->enqueue(o);
+  q2->enqueue(o);
+
+  result &= (q1->equals(q2));
+  result &= (q1->hash() == q2->hash());
+
+  q2->enqueue(o2);
+
+  result &= (!q1->equals(q2));
+  result &= (q1->hash() != q2->hash());
+
+  q1->clear();
+  q2->clear();
+
+  q1->enqueue(o);
+  q2->enqueue(o2);
+
+  result &= (!q1->equals(q2));
+  result &= (q1->hash() != q2->hash());
+
+  printf("%d\n", result);
+
+  delete q1;
+  delete q2;
+  delete o;
+  delete o2;
+  if (result) {
+    exit(0);
+  }
+  exit(4);
 }
 
-int main()
-{
-    Test1();
-    Test2();
-    Test3();
-    Test4();
-    Test5();
+TEST(W1, Test5) {
+  ASSERT_EXIT(Test5(), ::testing::ExitedWithCode(0), ".*");
+}
 
-    return 0;
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
